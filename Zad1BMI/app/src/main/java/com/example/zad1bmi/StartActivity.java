c29c01256a4487ffd9657bd8b61098cc1a12fe32
Madzialenna <s22622@pjwstk.edu.pl>
2025-03-31 22:05:27 +0200

    Start view with vector imge added

---
+++ Zad1BMI/app/src/main/java/com/example/zad1bmi/StartActivity.java
@@ -0,0 +1,27 @@
+package com.example.zad1bmi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Zamknij StartActivity po przejściu
+            }
+        });
+    }
+}
